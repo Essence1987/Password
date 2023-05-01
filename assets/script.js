81a98609e3275824de1a30582e2e509dc52b2200
Essence1987 <hwmelander@gmail.com>
2023-05-01 16:23:45 -0700

    Added Questions to ask user for Password

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var passLg = prompt("How many characters would you like your password to be?")
+  var incLC = confirm("Include lowercase letters?")
+  var incUC = confirm("Include uppercase letters?")
+  var incNum = confirm("includ numbers?")
+  var incSpc = confirm("Include special characters?")
+
+  var password = generatePassword(passLg, incLC, incUC, incNum, incSpc );
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
