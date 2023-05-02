233844e4552ffb0f3a57bf53e1a3a2ee38a06090
Essence1987 <hwmelander@gmail.com>
2023-05-01 17:14:46 -0700

    Added Validation Variables in order to fix Undefined Error when a user selects NO for every user prompt

--- assets/script.js
+++ assets/script.js
@@ -39,10 +39,23 @@ function generatePassword(passLg, incLC, incUC, incNum,incSpc) {
 function writePassword() {
 
   var passLg = prompt("How many characters would you like your password to be?")
+
+  if (passLg < 8 || passLg > 128) {
+    alert("Password length must be between 8 and 128.");
+    return;
+  }
+
+
   var incLC = confirm("Include lowercase letters?")
   var incUC = confirm("Include uppercase letters?")
   var incNum = confirm("includ numbers?")
   var incSpc = confirm("Include special characters?")
+
+
+  if (!incLC && !incUC && !incNum && !incSpc) {
+    alert("At least one character type must be selected.");
+    return;
+  }
 
   var password = generatePassword(passLg, incLC, incUC, incNum, incSpc);
   var passwordText = document.querySelector("#password");
